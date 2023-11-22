fbdaa5f6611d60c5d8b22ab99176467e237db2e4
Hegsina <hegelminniti@gmail.com>
2023-11-22 13:48:50 +0100

    Template Strings - Exercise 1

---
+++ template_strings1.js
@@ -0,0 +1,10 @@
+// Crea una template string che calcola e stampa in console la somma di due numeri.
+
+const num1 = 10;
+const num2 = 20;
+const sum = console.log(`La somma di ${num1} e ${num2} è ${num1 + num2}`);
+
+//oppure 
+
+const sum2 = `La somma di ${num1} e ${num2} è ${num1 + num2}`;
+console.log(sum2);
